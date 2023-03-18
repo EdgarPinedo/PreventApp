8639052d898b3549b34261a1c1ea35dfa26f605a
Edgar <edgarpinedo122@hotmail.com>
2023-03-18 06:12:41 -0600

    Cambie el mapa

--- PreventApp/wwwroot/js/map.js
+++ PreventApp/wwwroot/js/map.js
@@ -3,7 +3,6 @@ var mapOptions = {
     center: [20.6696, -103.3405],
     zoom: 13,
     minZoom:12,
-    maxZoom: 17,
     zoomControl: false,
     maxBounds: [
         //Suroeste
@@ -16,9 +15,15 @@ var mapOptions = {
 var map = L.map('map', mapOptions);
 L.control.zoom({ position: "bottomright" }).addTo(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Versión del mapa gratuita hasta 200k solicitudes al mes
+L.tileLayer('https://api.mapbox.com/styles/v1/edgarpinedo/clfdrg9r1007t01p5sg0iffv0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRnYXJwaW5lZG8iLCJhIjoiY2xmZHFtcTE2MGl2ejNycnJranlmd3I4ZyJ9.ocG7NBF7rlsU60Nq6WMbVQ', {
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> contributors`,
 }).addTo(map);
+
+// Versión del mapa gratuita sin límite
+/*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);*/
 
 map.on('click', function (e) {
     document.getElementById("latitud").innerHTML = e.latlng.lat;
